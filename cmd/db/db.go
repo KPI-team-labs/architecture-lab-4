1beb78fe9d6f2221477950df7babb94e7d7361f4
Maxim Sachko <114748761+SachkoMaxim@users.noreply.github.com>
2024-06-02 14:47:07 +0300

    added db.go and updated server.go

---
+++ cmd/db/db.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/KPI-team-labs/architecture-lab-4/datastore"
+	"github.com/KPI-team-labs/architecture-lab-4/httptools"
+	"github.com/KPI-team-labs/architecture-lab-4/signal"
+)
+
+var port = flag.Int("port", 8083, "server port")
+
+type RespBody struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type ReqBody struct {
+	Value string `json:"value"`
+}
+
+type server struct {
+	*http.ServeMux
+}
+
+func main() {
+	flag.Parse()
+
+	s := &server{ServeMux: http.NewServeMux()}
+	dir, err := ioutil.TempDir("", "temp-dir")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err := datastore.NewDb(dir, 250)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	s.HandleFunc("/db/", func(rw http.ResponseWriter, req *http.Request) {
+		handleDBRequest(rw, req, db)
+	})
+
+	httpServer := httptools.CreateServer(*port, s)
+	httpServer.Start()
+
+	signal.WaitForTerminationSignal()
+}
+
+func (s *server) Start() {
+	log.Printf("Server listening on port %d", *port)
+	err := http.ListenAndServe(":"+strconv.Itoa(*port), s)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleDBRequest(rw http.ResponseWriter, req *http.Request, Db *datastore.Db) {
+	log.Println("Caught request")
+	key := req.URL.Path[len("/db/"):]
+	log.Printf("Key: %s", key)
+
+	switch req.Method {
+	case http.MethodGet:
+		value, err := Db.Get(key)
+		if err != nil {
+			rw.WriteHeader(http.StatusNotFound)
+			return
+		}
+		rw.WriteHeader(http.StatusOK)
+		rw.Header().Set("content-type", "application/json")
+		_ = json.NewEncoder(rw).Encode(RespBody{
+			Key:   key,
+			Value: value,
+		})
+	case http.MethodPost:
+		var body ReqBody
+
+		err := json.NewDecoder(req.Body).Decode(&body)
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = Db.Put(key, body.Value)
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		rw.WriteHeader(http.StatusCreated)
+	default:
+		rw.WriteHeader(http.StatusBadRequest)
+	}
+}
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -16,9 +19,20 @@ var port = flag.Int("port", 8080, "server port")
 
 const confResponseDelaySec = "CONF_RESPONSE_DELAY_SEC"
 const confHealthFailure = "CONF_HEALTH_FAILURE"
+const dbUrl = "http://db:8083/db"
+
+type ReqBody struct {
+	Value string `json:"value"`
+}
+
+type RespBody struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
 
 func main() {
 	h := new(http.ServeMux)
+	client := http.DefaultClient
 
 	h.HandleFunc("/health", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Header().Set("content-type", "text/plain")
@@ -34,23 +48,77 @@ func main() {
 	report := make(Report)
 
 	h.HandleFunc("/api/v1/some-data", func(rw http.ResponseWriter, r *http.Request) {
-		respDelayString := os.Getenv(confResponseDelaySec)
-		if delaySec, parseErr := strconv.Atoi(respDelayString); parseErr == nil && delaySec > 0 && delaySec < 300 {
-			time.Sleep(time.Duration(delaySec) * time.Second)
+		key := r.URL.Query().Get("key")
+		if key != "" {
+			resp, err := client.Get(fmt.Sprintf("%s/%s", dbUrl, key))
+			statusOk := resp.StatusCode >= 200 && resp.StatusCode < 300
+			if err != nil {
+				rw.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			if !statusOk {
+				rw.WriteHeader(resp.StatusCode)
+				return
+			}
+			respDelayString := os.Getenv(confResponseDelaySec)
+			if delaySec, parseErr := strconv.Atoi(respDelayString); parseErr == nil && delaySec > 0 && delaySec < 300 {
+				time.Sleep(time.Duration(delaySec) * time.Second)
+			}
+
+			report.Process(r)
+
+			var body RespBody
+			json.NewDecoder(resp.Body).Decode(&body)
+
+			rw.Header().Set("content-type", "application/json")
+			rw.WriteHeader(http.StatusOK)
+			_ = json.NewEncoder(rw).Encode(body)
+
+			defer resp.Body.Close()
+		} else {
+			respDelayString := os.Getenv(confResponseDelaySec)
+			if delaySec, parseErr := strconv.Atoi(respDelayString); parseErr == nil && delaySec > 0 && delaySec < 300 {
+				time.Sleep(time.Duration(delaySec) * time.Second)
+			}
+
+			report.Process(r)
+
+			rw.Header().Set("content-type", "application/json")
+			rw.WriteHeader(http.StatusOK)
+
+			responseSize := 1024
+			if key == "" {
+				if sizeHeader := r.Header.Get("Response-Size"); sizeHeader != "" {
+					if size, err := strconv.Atoi(sizeHeader); err == nil && size > 0 {
+						responseSize = size
+					}
+				}
+
+				responseData := make([]string, responseSize)
+				for i := 0; i < responseSize; i++ {
+					responseData[i] = strconv.Itoa(responseSize)
+				}
+
+				_ = json.NewEncoder(rw).Encode(responseData)
+			}
 		}
 
-		report.Process(r)
-
-		rw.Header().Set("content-type", "application/json")
-		rw.WriteHeader(http.StatusOK)
-		_ = json.NewEncoder(rw).Encode([]string{
-			"1", "2",
-		})
 	})
 
 	h.Handle("/report", report)
 
 	server := httptools.CreateServer(*port, h)
 	server.Start()
+
+	buff := new(bytes.Buffer)
+	body := ReqBody{Value: time.Now().Format(time.RFC3339)}
+	json.NewEncoder(buff).Encode(body)
+
+	res, err := client.Post(fmt.Sprintf("%s/test", dbUrl), "application/json", buff)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
 	signal.WaitForTerminationSignal()
 }
